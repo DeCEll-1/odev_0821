e438bc258d49c636775e3d88bec9017d7c72b5ff
DeCell_ <poedx2@gmail.com>
2022-08-21 15:35:15 +0300

    ilk
    yorum
    a

---
+++ 0821odev/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0821odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Label lbl_isim = new Label();
+            this.Controls.Add(lbl_isim);
+            lbl_isim.AutoSize = true;
+            lbl_isim.Text = "İsim:";
+            lbl_isim.Location = new Point(250, 30);
+
+            Label lbl_soyisim = new Label();
+            lbl_soyisim.Text = "soyisim:";
+            lbl_soyisim.Location = new Point(250, 57);
+            this.Controls.Add(lbl_soyisim);
+
+            TextBox tb_isim = new TextBox();
+            tb_isim.Location = new Point(300, 30);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
